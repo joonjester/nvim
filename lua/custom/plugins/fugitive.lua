dc7bf68e76f40a6498df832fcca14ad62ace1494
hyunjoonchoi <hyunjoonchoi04@gmail.com>
2024-12-12 13:18:22 +0100

    fix: change somesett

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -3,24 +3,21 @@ return {
 	config = function()
 		vim.keymap.set("n", "<leader>gt", vim.cmd.Git)
 
-		vim.keymap.set("n", "<leader>ga", function()
-			vim.cmd.Git("add .")
-		end)
-
-		vim.keymap.set("n", "<leader>gc", function()
-			local message = vim.fn.input("Commit message: ")
-			vim.cmd('Git commit -m "' .. message .. '"')
-		end)
-
 		vim.keymap.set("n", "<leader>gl", function()
 			vim.cmd("Git log --oneline")
 		end)
 
 		local bufnr = vim.api.nvim_get_current_buf()
 		local opts = { buffer = bufnr, remap = false }
+		vim.keymap.set("n", "<leader>gc", function()
+			local message = vim.fn.input("Commit message: ")
+			vim.cmd('Git commit -m "' .. message .. '"')
+		end, opts)
+
 		vim.keymap.set("n", "<leader>p", function()
 			vim.cmd.Git({ "pull", "--rebase" })
 		end, opts)
+
 		vim.keymap.set("n", "<leader>P", function()
 			vim.cmd.Git("push")
 		end, opts)
--- lua/custom/plugins/terminal.lua
+++ lua/custom/plugins/terminal.lua
@@ -1,24 +1,9 @@
 return {
 	"voldikss/vim-floaterm",
 	config = function()
-		vim.keymap.set("n", "<leader>to", function()
-			vim.cmd("FloatermNew")
-		end)
-		vim.keymap.set("n", "<leader>tt", function()
-			vim.cmd("FloatermToggle")
-		end)
-		vim.keymap.set("n", "<leader>td", function()
-			vim.cmd("FloatermKill")
-			vim.cmd("FloatermToggle")
-		end)
-		vim.keymap.set("n", "<leader>tk", function()
-			vim.cmd("FloatermKill")
-		end)
-		vim.keymap.set("n", "<leader>tn", function()
-			vim.cmd("FloatermNext")
-		end)
-		vim.keymap.set("n", "<leader>tp", function()
-			vim.cmd("FloatermPrev")
-		end)
+		vim.keymap.set("n", "<C-t>", "<cmd>:FloatermToggle<CR>")
+		vim.keymap.set("t", "<C-t>", "<cmd>:FloatermToggle<CR>")
+		vim.keymap.set("t", "<C-l>", "<cmd>:FloatermNext<CR>")
+		vim.keymap.set("t", "<C-h>", "<cmd>:FloatermPrev<CR>")
 	end,
 }
